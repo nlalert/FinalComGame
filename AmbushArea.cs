b14836cb4794773c0b089d83d4283e57462d2ade
Natthakrit Lalert <99980994+nlalert@users.noreply.github.com>
2025-03-27 11:53:10 +0700

    fix enemy spawn outside expected area

--- AmbushArea.cs
+++ AmbushArea.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -7,28 +8,28 @@ namespace FinalComGame
 {
     public class AmbushArea
     {
-        private Rectangle triggerZone;
-        private Dictionary<Vector2, int> enemySpawnPoints;
-        private List<BaseEnemy> spawnedEnemies;
-        private bool isActive;
-        private bool isCleared;
+        private Rectangle _triggerZone;
+        private Dictionary<Vector2, int> _enemySpawnPoints;
+        private List<BaseEnemy> _spawnedEnemies;
+        private bool _isActive;
+        private bool _isCleared;
 
         SlimeEnemy slimeEnemy;
         public AmbushArea(Rectangle triggerZone, TileMap tileMap, SlimeEnemy tempSlime)
         {
             slimeEnemy = tempSlime;
-            this.triggerZone = triggerZone;
-            spawnedEnemies = new List<BaseEnemy>();
+            this._triggerZone = triggerZone;
+            _spawnedEnemies = new List<BaseEnemy>();
             
             // Find enemy spawn points within this area
-            enemySpawnPoints = new Dictionary<Vector2, int>();
+            _enemySpawnPoints = new Dictionary<Vector2, int>();
             foreach (var spawnPoint in tileMap.GetEnemySpawnPoints())
             {
                 Vector2 worldPosition = TileMap.GetTileWorldPositionAt(spawnPoint.Key);
                 if (triggerZone.Contains(worldPosition))
                 {
                     Console.WriteLine("Add : "+spawnPoint.Value);
-                    enemySpawnPoints.Add(spawnPoint.Key, spawnPoint.Value);
+                    _enemySpawnPoints.Add(spawnPoint.Key, spawnPoint.Value);
                 }
             }
         }
@@ -36,26 +37,21 @@ namespace FinalComGame
         public void Update(GameTime gameTime, List<GameObject> gameObjects, TileMap tileMap)
         {
             // Check if player is in the trigger zone
-            if (!isActive && triggerZone.Contains(Singleton.Instance.Player.Position))
+            if (!_isActive && _triggerZone.Contains(Singleton.Instance.Player.Position))
             {
                 ActivateAmbush(gameObjects, tileMap);
                 Console.WriteLine("Active!");
             }
 
             // Check if all enemies are defeated
-            if (!isCleared)
+            if (!_isCleared)
             {
-                spawnedEnemies.RemoveAll(enemy => !enemy.IsActive);
+                _spawnedEnemies.RemoveAll(enemy => !enemy.IsActive);
 
-                if (spawnedEnemies.Count == 0)
+                if (_spawnedEnemies.Count == 0)
                 {
-                    isCleared = true;
-                    //TODO : Optimize this
-                    foreach (var tile in tileMap.tiles)
-                    {
-                        if(tile.Value.Type == TileType.AmbushBarrier)
-                            tile.Value.IsSolid = false;
-                    }
+                    _isCleared = true;
+                    ChangeBarrierCollision(tileMap, false);
                     Console.WriteLine("Clear");
                 }
             }
@@ -63,22 +59,18 @@ namespace FinalComGame
 
         private void ActivateAmbush(List<GameObject> gameObjects, TileMap tileMap)
         {
-            isActive = true;
-            isCleared = false;
-            //TODO : Optimize this
-            foreach (var tile in tileMap.tiles)
-            {
-                if(tile.Value.Type == TileType.AmbushBarrier)
-                    tile.Value.IsSolid = true;
-            }
+            _isActive = true;
+            _isCleared = false;
 
-            foreach (var enemySpawnPoint in tileMap.GetEnemySpawnPoints())
+            ChangeBarrierCollision(tileMap, true);
+
+            foreach (var enemySpawnPoint in _enemySpawnPoints)
             {
                 switch (enemySpawnPoint.Value)
                 {
                     case 97:
                         // HellhoundEnemy.
-                        slimeEnemy.Spawn(TileMap.GetTileWorldPositionAt(enemySpawnPoint.Key), gameObjects, spawnedEnemies);
+                        slimeEnemy.Spawn(TileMap.GetTileWorldPositionAt(enemySpawnPoint.Key), gameObjects, _spawnedEnemies);
                         // _enemyDog.Spawn(TileMap.GetTileWorldPositionAt(enemySpawnPoint.Key), _gameObjects);
                         // _baseSkeleton.Spawn(TileMap.GetTileWorldPositionAt(enemySpawnPoint.Key), _gameObjects);
                         // _enemyDemon.Spawn(TileMap.GetTileWorldPositionAt(enemySpawnPoint.Key), _gameObjects);
@@ -93,19 +85,14 @@ namespace FinalComGame
             
         }
 
-        public bool CanPlayerLeave()
+        private static void ChangeBarrierCollision(TileMap tileMap, bool IsSolid)
         {
-            // Player can only leave if the ambush area is cleared
-            return isCleared;
-        }
-
-        public void Draw(SpriteBatch spriteBatch)
-        {
-            // Optional: Draw debug visualization of the ambush area
-            // This helps during development to see the trigger zone
-            Texture2D pixel = new Texture2D(spriteBatch.GraphicsDevice, 1, 1);
-            pixel.SetData(new[] { Color.Red * 0.3f });
-            spriteBatch.Draw(pixel, triggerZone, Color.White);
+            //TODO : Optimize this
+            foreach (var tile in tileMap.tiles)
+            {
+                if(tile.Value.Type == TileType.AmbushBarrier)
+                    tile.Value.IsSolid = IsSolid;
+            }
         }
     }
 }
