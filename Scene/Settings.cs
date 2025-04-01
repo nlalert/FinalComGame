1a255ad0e5bce96d8dc049b83ba3f7ae6de80576
Natthakrit Lalert <99980994+nlalert@users.noreply.github.com>
2025-04-01 15:26:50 +0700

    Add sound effect slider

--- Scene/Settings.cs
+++ Scene/Settings.cs
@@ -4,13 +4,16 @@ using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Audio;
 
 namespace FinalComGame;
 
 public class Settings : Scene
 {
     private TextUI _settingsTitle; // can change to ImageUI later
-    private SlideBarUI _masterVolumeSlider;
+    private SlideBarUI _musicVolumeSlider;
+    private SlideBarUI _soundEffectVolumeSlider;
     private Button _musicSlideChip;
     private Button _sfxSlideChip;
     private Button _backButton;
@@ -90,165 +93,13 @@ public class Settings : Scene
     public override void Update(GameTime gameTime)
     {
         base.Update(gameTime);
-        // if (!_settings)
-        // {
 
-        //     // Unpause when left clicked & released on resume button or pressed & released "Escape key"
-        //     if (_resumeButton.IsClicked() || (Singleton.Instance.CurrentKey.IsKeyDown(Keys.Escape) && Singleton.Instance.CurrentKey != Singleton.Instance.PreviousKey)){
-        //         Singleton.Instance.CurrentGameState = Singleton.Instance.PreviousGameState;
-        //     }
+        Singleton.Instance.MusicVolume = _musicVolumeSlider.GetValue()/100;
+        Singleton.Instance.SFXVolume = _soundEffectVolumeSlider.GetValue()/100;
 
-        //     // Restart to stage 1 when left clicked & released on restart button
-        //     if (_restartButton.IsClicked())
-        //     {
-        //         Singleton.Instance.CurrentGameState = Singleton.GameState.StartingGame;
-        //     }
-
-        //     // Show settings when left clicked & released on settings button
-        //     if (_settingsButton.IsClicked())
-        //     {
-        //         _settings = true;
-        //     }
-
-        //     // Exit to main-menu when left clicked & released on main-menu button
-        //     if (_mainmenuButton.IsClicked())
-        //     {
-        //         Singleton.Instance.CurrentGameState = Singleton.GameState.MainMenu;
-        //     }
-
-        // }
-
-        // else
-        // {
-
-        //     // Exit to pause menu when left clicked & released on back cutton or pressed & released "Escape key"
-        //     if (_backButton.IsClicked() || Singleton.Instance.CurrentKey.IsKeyDown(Keys.Escape) && Singleton.Instance.CurrentKey != Singleton.Instance.PreviousKey)
-        //     {
-        //         _settings = false;
-        //     }
-
-        //     // Prevent dragging boyh chips at the same time
-        //     if (!_sfxSlideChip.Dragging)
-        //     {
-        //     _musicSlideChip.Update(gameTime, _gameObjects);
-        //     }
-        //     if (!_musicSlideChip.Dragging)
-        //     {
-        //     _sfxSlideChip.Update(gameTime, _gameObjects);
-        //     }
-
-        //     // Adjust music volume by left click and drag the music slide chip
-        //     if (_musicSlideChip.Dragging)
-        //     {
-
-        //         int newX = Math.Clamp(Singleton.Instance.CurrentMouseState.X - (_musicSlideChip.Viewport.Width / 2), (Singleton.SCREEN_WIDTH / 2) - (Singleton.CHIP_SIZE/ 2) - _slideBarMaxValue/2, (Singleton.SCREEN_WIDTH / 2) - (Singleton.CHIP_SIZE/ 2) + _slideBarMaxValue/2);
-        //         _musicSlideChip.Position.X = newX;
-
-        //         float _slideBarMinPosition = (Singleton.SCREEN_WIDTH / 2) - (Singleton.CHIP_SIZE/ 2) - (_slideBarMaxValue/2);
-        //         _musicSlideChipPosition = _musicSlideChip.Position.X - _slideBarMinPosition;
-        //         Singleton.Instance.MusicVolume = _musicSlideChipPosition / _slideBarMaxValue;
-
-        //     }
-            
-        //     // Adjust sfx volume by left click and drag the sfx slide chip
-        //     if (_sfxSlideChip.Dragging)
-        //     {
-
-        //         int newX = Math.Clamp(Singleton.Instance.CurrentMouseState.X - (_sfxSlideChip.Viewport.Width / 2), (Singleton.SCREEN_WIDTH / 2) - (Singleton.CHIP_SIZE/ 2) - _slideBarMaxValue/2, (Singleton.SCREEN_WIDTH / 2) - (Singleton.CHIP_SIZE/ 2) + _slideBarMaxValue/2);
-        //         _sfxSlideChip.Position.X = newX;
-
-        //         float _slideBarMinPosition = (Singleton.SCREEN_WIDTH / 2) - (Singleton.CHIP_SIZE/ 2) - (_slideBarMaxValue/2);
-        //         _sfxSlideChipPosition = _sfxSlideChip.Position.X - _slideBarMinPosition;
-        //         //Singleton.Instance.Volume = _sfxSlideChipPosition / _slideBarMaxValue;
-
-        //     }
-
-        //     // Music slide chip base on music volume
-        //     if (Singleton.Instance.MusicVolume <= 0)
-        //     {
-        //         _musicSlideChip.Viewport = ViewportManager.Get("Slide_Chip0");
-        //     }
-        //     else if (Singleton.Instance.MusicVolume <= 0.33)
-        //     {
-        //         _musicSlideChip.Viewport = ViewportManager.Get("Slide_Chip1");
-        //     }
-        //     else if (Singleton.Instance.MusicVolume <= 0.66)
-        //     {
-        //         _musicSlideChip.Viewport = ViewportManager.Get("Slide_Chip2");
-        //     }
-        //     else
-        //     {
-        //         _musicSlideChip.Viewport = ViewportManager.Get("Slide_Chip3");             
-        //     }
-
-        //     // SFX slide chip base on SFX volume
-        //     if (Singleton.Instance.SFXVolume <= 0)
-        //     {
-        //         _sfxSlideChip.Viewport = ViewportManager.Get("Slide_Chip0");
-        //     }
-        //     else if (Singleton.Instance.SFXVolume <= 0.33)
-        //     {
-        //         _sfxSlideChip.Viewport = ViewportManager.Get("Slide_Chip1");
-        //     }
-        //     else if (Singleton.Instance.SFXVolume <= 0.66)
-        //     {
-        //         _sfxSlideChip.Viewport = ViewportManager.Get("Slide_Chip2");
-        //     }
-        //     else
-        //     {
-        //         _sfxSlideChip.Viewport = ViewportManager.Get("Slide_Chip3");             
-        //     }
-
-        // }
+        MediaPlayer.Volume = Singleton.Instance.MusicVolume; 
+        SoundEffect.MasterVolume = Singleton.Instance.SFXVolume;
     }
-
-    // public void Draw(GameTime gameTime)
-    // {
-        // // Tranparent background
-        // _spriteBatch.Draw(_rectTexture, Vector2.Zero, new Rectangle(0, 0, Singleton.SCREEN_WIDTH, Singleton.SCREEN_HEIGHT), new Color(0, 0, 0, 150));
-
-        // if (!_settings)
-        // {         
-        //     // Pause Title
-        //     _spriteBatch.Draw(_texture, new Vector2((Singleton.SCREEN_WIDTH / 2) - (ViewportManager.Get("Pause_Title0").Width / 2), 
-        //     _pauseTitleHeight - (ViewportManager.Get("Pause_Title0").Height / 2)), ViewportManager.Get("Pause_Title0"), Color.White);
-
-        //     // Buttons
-        //     _resumeButton.Draw(_spriteBatch);
-        //     _restartButton.Draw(_spriteBatch);
-        //     _settingsButton.Draw(_spriteBatch);
-        //     _mainmenuButton.Draw(_spriteBatch);
-
-        // }
-
-        // else {
-
-        //     // Settings Box
-        //     _spriteBatch.Draw(_texture, new Vector2((Singleton.SCREEN_WIDTH / 2) - (ViewportManager.Get("Big_Box0").Width/ 2),
-        //      (Singleton.SCREEN_HEIGHT / 2) - (ViewportManager.Get("Big_Box0").Height/ 2)), ViewportManager.Get("Big_Box0"), Color.White);
-            
-        //     // Music
-        //     _spriteBatch.Draw(_texture, new Vector2((Singleton.SCREEN_WIDTH / 2) - (ViewportManager.Get("Music_Label").Width/ 2),
-        //     _musicLabelHeight), ViewportManager.Get("Music_Label"), Color.White);
-
-        //     _spriteBatch.Draw(_texture, new Vector2((Singleton.SCREEN_WIDTH / 2) - (ViewportManager.Get("Slide_Bar").Width/ 2),
-        //     _musicSlideBarHeight), ViewportManager.Get("Slide_Bar"), Color.White);
-            
-        //     // SFX
-        //     _spriteBatch.Draw(_texture, new Vector2((Singleton.SCREEN_WIDTH / 2) - (ViewportManager.Get("SFX_Label").Width/ 2),
-        //     _sfxLabelHeight), ViewportManager.Get("SFX_Label"), Color.White);
-
-        //     _spriteBatch.Draw(_texture, new Vector2((Singleton.SCREEN_WIDTH / 2) - (ViewportManager.Get("Slide_Bar").Width/ 2),
-        //     _sfxSlideBarHeight), ViewportManager.Get("Slide_Bar"), Color.White);
-            
-        //     // Back button
-        //     _backButton.Draw(_spriteBatch);
-
-        //     // Slide Chip
-        //     _musicSlideChip.Draw(_spriteBatch);
-        //     _sfxSlideChip.Draw(_spriteBatch);
-        // }
-    // }
 
     protected override void SetupHUD()
     {
@@ -267,14 +118,25 @@ public class Settings : Scene
         int SliderHeight = 30;
         Texture2D SliderBar = _content.Load<Texture2D>("ItemSlot");//TEMP
         Texture2D SliderHandle =   _content.Load<Texture2D>("sliderHandle"); // 32x32
-        _masterVolumeSlider = new SlideBarUI(
+        _musicVolumeSlider = new SlideBarUI(
             new Rectangle((Singleton.SCREEN_WIDTH - SliderWidth) / 2 , (Singleton.SCREEN_HEIGHT - SliderHeight) * 2 / 7, SliderWidth, SliderHeight),
-            "Master Volume",
+            "Music Volume",
             SliderBar,
             SliderHandle,
             0,    // min value
             100,  // max value
-            50,   // start value
+            75,   // start value
+            "{0}%" // value format
+        );
+
+        _soundEffectVolumeSlider = new SlideBarUI(
+            new Rectangle((Singleton.SCREEN_WIDTH - SliderWidth) / 2 , (Singleton.SCREEN_HEIGHT - SliderHeight) * 3 / 7, SliderWidth, SliderHeight),
+            "Sound Effect Volume",
+            SliderBar,
+            SliderHandle,
+            0,    // min value
+            100,  // max value
+            75,   // start value
             "{0}%" // value format
         );
 
@@ -291,27 +153,9 @@ public class Settings : Scene
         _backButton.OnClick += BackButton_OnClick;
 
         _ui.AddHUDElement(_settingsTitle);
-        _ui.AddHUDElement(_masterVolumeSlider);
+        _ui.AddHUDElement(_musicVolumeSlider);
+        _ui.AddHUDElement(_soundEffectVolumeSlider);
         _ui.AddHUDElement(_backButton);
-
-        // _musicSlideChip = new Button(_texture)
-        // {
-        //     Name = "MusicSlideChip",
-        //     Viewport = ViewportManager.Get("Slide_Chip0"),
-        //     Position = new Vector2((Singleton.SCREEN_WIDTH - ViewportManager.Get("Slide_Chip0").Width) / 2 + (int)(Singleton.Instance.MusicVolume*_slideBarMaxValue) - (_slideBarMaxValue/2),
-        //             _musicSlideChipHeight),
-        //     IsActive = true
-        // };
-
-        // _sfxSlideChip = new Button(_texture)
-        // {
-        //     Name = "SFXSlideChip",
-        //     Viewport = ViewportManager.Get("Slide_Chip0"),
-        //     Position = new Vector2((Singleton.SCREEN_WIDTH - ViewportManager.Get("Slide_Chip0").Width) / 2 + (int)(Singleton.Instance.SFXVolume*_slideBarMaxValue) - (_slideBarMaxValue/2),
-        //             _sfxSlideChipHeight),
-        //     IsActive = true
-        // };
-
     }
 
     private void BackButton_OnClick(object sender, EventArgs e)
